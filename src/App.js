d0c7e7c86090b6d81bee0da2e2adb37b6d86fde0
IntjEpicLeader <ngocpm9911@gmail.com>
2021-02-08 16:26:29 +0700

    add shop page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import HomePage from "./pages/homepage/homepage.component";
 import "./styles.css";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
+const HatsPage = ({ history }) => {
+  return (
+    <div>
+      <h1>Hat Page</h1>
+    </div>
+  );
+};
 export default function App() {
   return (
     <div className="App">
-      <HomePage />;
+      <BrowserRouter>
+        <Header />
+
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
